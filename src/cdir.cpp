c22ad0c712e2ba9b6a023b0233837fbf9a7ddcfd
Maxi <voynich@disroot.org>
2023-11-08 21:10:01 -0500

    directory deletion

--- src/cdir.cpp
+++ src/cdir.cpp
@@ -10,11 +10,17 @@
 #include "lib/trilutil.h" // no-arg handling
 
 int main(int argc, char *argv[]) { 
-  if (arg_check(argc, 2) == false) {
-        std::cout << "[FATAL] no directory name supplied :3\n";
+    if (arg_check(argc, 1) == false) {
+        std::cout << "[FATAL] most likely no directory name supplied :3\n";
         return 1;
     }
-    std::filesystem::create_directory(argv[1]);
+    std::string dir_arg = argv[1];
+    if (dir_arg == "-r") {
+        // remove directory if -r option supplied
+        std::filesystem::remove(argv[2]);
+        return 0;
+    }
 
+    std::filesystem::create_directory(dir_arg);
     return 0;
 }
--- src/lib/trilutil.h
+++ src/lib/trilutil.h
@@ -1,7 +1,7 @@
 #include <iostream>
 
-bool arg_check(int argc, int expected) {
-    if (argc == expected) {
+bool arg_check(int argc, int bad) {
+    if (argc != bad) {
         return true;
     }
     return false;
